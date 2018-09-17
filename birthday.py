ee0969d464e5204b00b0e1b06020c3ed681a5219
ellaedmonds <35499735+ellaedmonds@users.noreply.github.com>
2018-09-17 11:39:47 -0400

    Updated from Brython Server: 9/17/2018 11:39:45 AM

--- birthday.py
+++ birthday.py
@@ -31,3 +31,12 @@ Example Session
   And the day? 11
   Eric, you are a fall baby of the stone age.
 """
+name = input ('Hello, what is your name? ')
+a = input (str('Hi '+name+', what was the name of the month you were born? '))
+b = float (input ('And what year were you born in, '+name+'? '))
+c = float (input ('And the day? '))
+
+if a is 'october' and c == 31:
+    print('You were born on Halloween!')
+if a is 'september' and c == 17:
+    print('Happy Birthday!')
